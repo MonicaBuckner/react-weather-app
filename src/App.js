d5fa7fd477535eca4e449d0c8eb12d60aa92c697
MonicaBuckner <74023919+MonicaBuckner@users.noreply.github.com>
2021-01-21 13:39:30 -0800

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
     
      <h1>Weather App</h1>
+     <footer> <a href="https://github.com/MonicaBuckner/react-weather-app" target="_blank">
+          Open-sourced code{" "}
+        </a>
+        by{" "}
+        <a href="https://www.linkedin.com/in/monica-buckner/" target="_blank">
+          Monica Buckner
+        </a></footer>
     </div>
   );
 }
-
-export default App;
