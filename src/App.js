1c1f3ed0c174586d9e12aa25c9459e60bd12b442
MonicaBuckner <74023919+MonicaBuckner@users.noreply.github.com>
2021-01-21 14:25:23 -0800

    Called Weather component in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
     
-     <h1>Weather App</h1>
+     <Weather />
      <footer> 
        <a href="https://github.com/MonicaBuckner/react-weather-app" target="_blank" rel="noreferrer">
           Open-sourced code{" "}
